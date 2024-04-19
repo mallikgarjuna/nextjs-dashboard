d7f23601d35e8be0177a085a6e934395ebc9f15d
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-04-19 18:29:06 +0200

    Ch14 Improving accessibility - Invoive form server-side validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,25 +7,46 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Please enter an amount greater than 0',
+  }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select a valid invoice status',
+  }),
   date: z.string(),
 });
 
 // Create Invoice
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+  // console.log(validatedFields); // has 'success' and 'data' properties
 
-  const parsedFormData = CreateInvoice.parse(rawFormData);
-  const { customerId, amount, status } = parsedFormData;
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields. Failed to create invoice.',
+    };
+  }
 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -75,7 +96,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // Delete Invoice
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to delete invoice');
+  // throw new Error('Failed to delete invoice');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
